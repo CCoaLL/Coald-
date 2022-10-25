31353c0cbcd890ffc9bfc40f6d54e8cb9fabc596
CCoaLL <94953851+CCoaLL@users.noreply.github.com>
2022-10-26 07:19:35 +0900

    ?

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 const express = require("express")
-const WebSocketServer = require("ws")
+const { WebSocketServer } = require("ws")
 const app = express()
+const session = require('express-session')
+const fileStore = require('session-file-store')(session)
+const bodyParser = require('body-parser')
+const fs = require("fs")
+
+app.use(
+  session({
+    secret: 'i6a43ah4cwxxpiibt4ws2snkran7i8augj4g6z3079f3fvpb0uxb02',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 6 * 60 * 10000,
+    },
+    store: new fileStore(),
+  }),
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/templates/app/index.html')
+  if(req.session.logged_in != true) {
+    return res.redirect('/login')
+  }
+    
+  return res.sendFile(__dirname + '/templates/app/index.html')
+})
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+  return res.sendFile(__dirname + '/templates/login/index.html')
+})
+
+app.post('/login', (req, res) => {
+  var email = req.body.email
+  var pw = req.body.pw
+  var db = JSON.parse(fs.readFileSync(__dirname + '/db/db.json'))
+  var user = null
+
+  for(item in db) {
+    if(db[item].email == email && db[item].pw == pw) {
+      user = item
+    }
+  }
+
+  if(user != null) {
+    req.session.logged_in=true
+    return res.redirect('/')
+  } else
+    return res.redirect('/login')
+})
+
+app.get('/register', (req, res) => {
+  return res.sendFile(__dirname + '/templates/register/index.html')
+})
+
+app.post('/register', (req, res) => {
+  var username = req.body.username
+  var email = req.body.email
+  var pw = req.body.pw
+  var role = 'human'
+  var id = Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)+Math.random().toString(36).substring(2, 11)
+  var db = JSON.parse(fs.readFileSync(__dirname + '/db/db.json'))
+  db[username] = {
+    "role": role,
+    "id": id,
+    "username": username,
+    "pw": pw,
+    "email": email
+  }
+
+  var du = JSON.stringify(db, null, 4);
+  fs.writeFileSync(__dirname + '/db/db.json', du)
+
+  return res.redirect('/')
 })
 
 app.listen(8000, () => {
-  console.log(`Example app listening on port 8000`)
+  console.log(`lop8000`)
 })
 
 const wss = new WebSocketServer({ port: 8001 })
@@ -25,9 +101,7 @@ wss.on("connection", (ws, request) => {
     })
   })
 
-  wss.clients.forEach(client => {
+  wss.clients.forEach((client => {
     client.send(`새로운 유저가 접속했습니다. 현재 유저 ${wss.clients.size} 명`)
-  })
-  
-  console.log(`새로운 유저 접속: ${request.socket.remoteAddress}`)
+  }))
 })
